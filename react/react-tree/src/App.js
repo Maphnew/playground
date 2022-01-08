395d19dc7f8cf3d943bfc2149cfc415f6573d0cc
maphnew <zcm3118@gmail.com>
2022-01-08 23:29:02 +0900

    03 title이 만약 컴포넌트라면

--- react/react-tree/src/App.js
+++ react/react-tree/src/App.js
@@ -1,106 +1,119 @@
 import { useState } from "react";
-import { Tree, Input } from "antd";
+import { Tree, Input, Checkbox } from "antd";
 
 const { Search } = Input;
 
-// const x = 3;
-// const y = 2;
-// const z = 1;
-// const gData = [];
-
-// const generateData = (_level, _preKey, _tns) => {
-//   const preKey = _preKey || "0";
-//   const tns = _tns || gData;
-
-//   const children = [];
-//   for (let i = 0; i < x; i++) {
-//     // 3 - 3개의 노드를 만들 것이다.
-//     const key = `${preKey}-${i}`;
-//     tns.push({ title: key, key });
-//     if (i < y) {
-//       // 2 - 2로 끝나는 노드 부터는 children이 없다.
-//       children.push(key);
-//     }
-//   }
-//   if (_level < 0) {
-//     return tns;
-//   }
-//   const level = _level - 1;
-//   children.forEach((key, index) => {
-//     tns[index].children = [];
-//     return generateData(level, key, tns[index].children);
-//   });
-// };
-// generateData(z); // 1 - tree의 depth가 2단계이다.
+const CheckboxNode = (props) => {
+  const { title } = props;
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <span>{title}</span>
+      <Checkbox.Group options={["learned", "used", "fluent"]} />
+    </div>
+  );
+};
 
 const gData = [
   {
     key: "0-0",
     title: "TypeScript",
+    unvisibleTitle: "TypeScript",
     children: [
       {
         key: "0-0-0",
         title: "Type",
+        unvisibleTitle: "Type",
         children: [
-          { key: "0-0-0-0", title: "boolean" },
-          { key: "0-0-0-1", title: "number" },
-          { key: "0-0-0-2", title: "string" },
+          {
+            key: "0-0-0-0",
+            unvisibleTitle: "boolean",
+            title: "boolean",
+          },
+          { key: "0-0-0-1", unvisibleTitle: "number", title: "number" },
+          { key: "0-0-0-2", unvisibleTitle: "string", title: "string" },
         ],
       },
       {
         key: "0-0-1",
         title: "Interface",
+        unvisibleTitle: "Interface",
         children: [
-          { key: "0-0-1-0", title: "implements" },
-          { key: "0-0-1-1", title: "optional-property" },
-          { key: "0-0-1-2", title: "indexable" },
+          { key: "0-0-1-0", unvisibleTitle: "implements", title: "implements" },
+          {
+            key: "0-0-1-1",
+            unvisibleTitle: "optional-property",
+            title: "optional-property",
+          },
+          { key: "0-0-1-2", unvisibleTitle: "indexable", title: "indexable" },
         ],
       },
       {
         key: "0-0-2",
         title: "Generic",
+        unvisibleTitle: "Generic",
       },
     ],
   },
   {
     key: "0-1",
     title: "TDD",
+    unvisibleTitle: "TDD",
     children: [
       {
         key: "0-1-0",
         title: "RED",
+        unvisibleTitle: "RED",
         children: [
-          { key: "0-1-0-0", title: "Write-a-test" },
-          { key: "0-1-0-1", title: "Test-fails" },
-          { key: "0-1-0-2", title: "Try-to-pass" },
+          {
+            key: "0-1-0-0",
+            unvisibleTitle: "Write-a-test",
+            title: "Write-a-test",
+          },
+          { key: "0-1-0-1", unvisibleTitle: "Test-fails", title: "Test-fails" },
+          {
+            key: "0-1-0-2",
+            unvisibleTitle: "Try-to-pass",
+            title: "Try-to-pass",
+          },
         ],
       },
       {
         key: "0-1-1",
         title: "GREEN",
+        unvisibleTitle: "GREEN",
         children: [
-          { key: "0-1-1-0", title: "Wrtie-code-make-test-pass" },
-          { key: "0-1-1-1", title: "code-passes-test" },
-          { key: "0-1-1-2", title: "Say-yeah" },
+          {
+            key: "0-1-1-0",
+            unvisibleTitle: "Wrtie-code-make-test-pass",
+            title: "Wrtie-code-make-test-pass",
+          },
+          {
+            key: "0-1-1-1",
+            unvisibleTitle: "code-passes-test",
+            title: "code-passes-test",
+          },
+          { key: "0-1-1-2", unvisibleTitle: "Say-yeah", title: "Say-yeah" },
         ],
       },
       {
         key: "0-1-2",
         title: "REFACTOR",
+        unvisibleTitle: "REFACTOR",
       },
     ],
   },
   {
     key: "0-2",
     title: "SCSS",
+    unvisibleTitle: "SCSS",
   },
 ];
 const dataList = [];
 const generateList = (data) => {
   for (let i = 0; i < data.length; i++) {
     const node = data[i];
-    const { key, title } = node;
-    dataList.push({ key, title });
+    const { key, title, unvisibleTitle } = node;
+    dataList.push({ key, title, unvisibleTitle });
     if (node.children) {
       generateList(node.children);
     }
@@ -128,9 +141,6 @@ const App = () => {
   const [searchValue, setSearchValue] = useState("");
   const [autoExpandParent, setAutoExpandParent] = useState(true);
 
-  console.log("gData", gData);
-  console.log("dataList", dataList);
-
   const onExpand = (expandedKeys) => {
     setExpandedKeys(expandedKeys);
     setAutoExpandParent(false);
@@ -140,7 +150,7 @@ const App = () => {
     const { value } = e.target;
     const expandedKeys = dataList
       .map((item) => {
-        if (item.title.indexOf(value) > -1) {
+        if (item.unvisibleTitle.indexOf(value) > -1) {
           return getParentKey(item.key, gData);
         }
         return null;
@@ -151,12 +161,12 @@ const App = () => {
     setAutoExpandParent(true);
   };
 
-  const loop = (data) =>
-    data.map((item) => {
-      const index = item.title.indexOf(searchValue);
-      const beforeStr = item.title.substr(0, index);
-      const afterStr = item.title.substr(index + searchValue.length);
-      const title =
+  const loop = (data) => {
+    return data.map((item) => {
+      const index = item.unvisibleTitle.indexOf(searchValue);
+      const beforeStr = item.unvisibleTitle.substr(0, index);
+      const afterStr = item.unvisibleTitle.substr(index + searchValue.length);
+      const unvisibleTitle =
         index > -1 ? (
           <span>
             {beforeStr}
@@ -166,15 +176,23 @@ const App = () => {
         ) : (
           <span>{item.title}</span>
         );
+      const title = <CheckboxNode title={unvisibleTitle} />;
       if (item.children) {
-        return { title, key: item.key, children: loop(item.children) };
+        return {
+          title: unvisibleTitle,
+          unvisibleTitle,
+          key: item.key,
+          children: loop(item.children),
+        };
       }
 
       return {
         title,
+        unvisibleTitle,
         key: item.key,
       };
     });
+  };
 
   return (
     <div>
@@ -188,6 +206,7 @@ const App = () => {
         expandedKeys={expandedKeys}
         autoExpandParent={autoExpandParent}
         treeData={loop(gData)}
+        blockNode={true}
       />
     </div>
   );
