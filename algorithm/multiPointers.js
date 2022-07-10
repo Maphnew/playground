082294c546bb1703f8367384924b0c5b98b10e1f
maphnew <zcm3118@gmail.com>
2022-07-10 22:37:45 +0900

    averagePair

--- algorithm/multiPointers.js
+++ algorithm/multiPointers.js
@@ -15,10 +15,10 @@ function countUniqueValues(array) {
   return result;
 }
 
-console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 2])); // 2
-console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
-console.log(countUniqueValues([])); // 0
-console.log(countUniqueValues([-2, -1, -1, 0, 1])); // 4
+// console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 2])); // 2
+// console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
+// console.log(countUniqueValues([])); // 0
+// console.log(countUniqueValues([-2, -1, -1, 0, 1])); // 4
 
 // Colt's Solution
 
@@ -34,10 +34,10 @@ function countUniqueValuesByColt(arr) {
   return i + 1;
 }
 
-console.log(countUniqueValuesByColt([1, 1, 1, 1, 1, 1, 2])); // 2
-console.log(countUniqueValuesByColt([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
-console.log(countUniqueValuesByColt([])); // 0
-console.log(countUniqueValuesByColt([-2, -1, -1, 0, 1])); // 4
+// console.log(countUniqueValuesByColt([1, 1, 1, 1, 1, 1, 2])); // 2
+// console.log(countUniqueValuesByColt([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
+// console.log(countUniqueValuesByColt([])); // 0
+// console.log(countUniqueValuesByColt([-2, -1, -1, 0, 1])); // 4
 
 function maxSubarraySum(arr, num) {
   let maxSum = 0;
@@ -54,7 +54,7 @@ function maxSubarraySum(arr, num) {
   return maxSum;
 }
 
-maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3); // 19
+// maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3); // 19
 
 // Divide and Conquer
 function search(array, val) {
@@ -76,10 +76,10 @@ function search(array, val) {
   return -1;
 }
 
-const result1 = search([1, 2, 3, 4, 5, 6], 4); // 3
-const result2 = search([1, 2, 3, 4, 5, 6], 6); // 5
-const result3 = search([1, 2, 3, 4, 5, 6], 11); // -1
-console.log(result1, result2, result3);
+const search1 = search([1, 2, 3, 4, 5, 6], 4); // 3
+const search2 = search([1, 2, 3, 4, 5, 6], 6); // 5
+const search3 = search([1, 2, 3, 4, 5, 6], 11); // -1
+// console.log(search1, search2, search3);
 
 // - Average Pair
 
@@ -91,15 +91,23 @@ console.log(result1, result2, result3);
 // Time: O(N)
 // Space: O(1)
 
-function averagePair(array, target) {
-  return true;
+function averagePair(arr, num) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    let avg = (arr[start] + arr[end]) / 2;
+    if (avg === num) return true;
+    else if (avg < num) start++;
+    else end--;
+  }
+  return false;
 }
 
-const result1 = averagePair([1, 2, 3], 2.5); // true
-const result2 = averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
-const result3 = averagePair([-1, 0, 3, 4, 5, 6], 4.1); // false
-const result4 = averagePair([], 4); // false
-console.log(result1, result2, result3, result4);
+const averagePair1 = averagePair([1, 2, 3], 2.5); // true
+const averagePair2 = averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
+const averagePair3 = averagePair([-1, 0, 3, 4, 5, 6], 4.1); // false
+const averagePair4 = averagePair([], 4); // false
+console.log(averagePair1, averagePair2, averagePair3, averagePair4);
 
 // - isSubsequence
 
@@ -110,11 +118,21 @@ const isSub1 = isSubsequence("hello", "hello world"); // true
 const isSub2 = isSubsequence("sing", "sting"); // true
 const isSub3 = isSubsequence("abc", "abracadabra"); // true
 const isSub4 = isSubsequence("abc", "acb"); // false (order matters)
+// console.log(isSub1, isSub2, isSub3, isSub4);
 
 // AT LEAST
 // Time Complexity - O(N + M)
 // Space Complexity - O(1)
 
-function isSubsequence(a, b) {
-  return true;
+function isSubsequence(str1, str2) {
+  var i = 0;
+  for (var j = 0; j < str2.length; j++) {
+    if (str1[i] === str2[j]) {
+      i++;
+    }
+    if (str1.length === i) {
+      return true;
+    }
+  }
+  return false;
 }
