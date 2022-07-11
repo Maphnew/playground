9a622111fba0abc6aa7a0a4233ad46b4538ecc67
maphnew <zcm3118@gmail.com>
2022-07-11 23:16:47 +0900

    maxSubarraySum

---
+++ algorithm/sllidingWindow.js
@@ -0,0 +1,33 @@
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) {
+    console.log(null);
+    return null;
+  }
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  console.log(maxSum);
+  return maxSum;
+}
+
+maxSubarraySum([100, 200, 300, 400], 2); // 700
+maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4); // 39
+maxSubarraySum([-3, 4, 0, -2, 6, -1], 2); // 5
+maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2); // 5
+maxSubarraySum([2, 3], 3); // null
+
+function minSubArrayLen() {}
+minSubArrayLen([2, 3, 1, 2, 4, 3], 7); // 2  -> because [4,3] is the smallest subarray
+minSubArrayLen([2, 1, 6, 5, 4], 9); // 2  -> because [5,4] is the smallest subarray
+minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52); // 1  -> because [62] is greater than 52
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39); // 3
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55); // 5
+minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11); // 2
+minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95); // 0
