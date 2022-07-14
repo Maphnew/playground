44dc5f3e2b35343d62be34a48bf7af052624564a
maphnew <zcm3118@gmail.com>
2022-07-15 06:42:32 +0900

    minSubArrayLen explaination

--- algorithm/sllidingWindow.js
+++ algorithm/sllidingWindow.js
@@ -23,6 +23,10 @@ maxSubarraySum([-3, 4, 0, -2, 6, -1], 2); // 5
 maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2); // 5
 maxSubarraySum([2, 3], 3); // null
 
+// minSubArrayLen
+// Parameters - an array of positive integers and a positive integer
+// This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+
 function minSubArrayLen() {}
 minSubArrayLen([2, 3, 1, 2, 4, 3], 7); // 2  -> because [4,3] is the smallest subarray
 minSubArrayLen([2, 1, 6, 5, 4], 9); // 2  -> because [5,4] is the smallest subarray
